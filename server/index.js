2f7b02fbd5da75460c4f2ca3d2686b41ea8426fb
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-05-25 12:50:26 -0600

    finished react server database setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const controller = require('./controller/controller');
+const controller = require('./controller');
 const app = express();
+const massive = require('massive');
+require('dotenv').config();
 
 app.use(bodyParser.json());
 
-app.post( '/api/product', controller.create );
-app.get( '/api/products', controller.read );
-app.put( '/api/product/:id', controller.update );
-app.delete( '/api/product/:id', controller.delete );
+
+massive(process.env.CONNECTION_STRING).then(dbInstance=>{
+    app.set('db', dbInstance);
+})
+
+const port = process.env.PORT || 3500;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}.`)
+});
+
+
+
+// app.post( '/api/products', controller.create);
+// app.get( '/api/products' , controller.getOne);
+// app.put( '/api/products/:id', controller.update );
+// app.delete( '/api/products/:id', controller.delete);
